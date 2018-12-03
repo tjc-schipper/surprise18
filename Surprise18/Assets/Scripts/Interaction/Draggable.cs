15515dfddcd56a059a6734ee2680d0bfb88c5c11
tjc-schipper <tjc.schipper@gmail.com>
2018-12-03 21:08:28 +0100

    Fixed snapping.

--- Surprise18/Assets/Scripts/Interaction/Draggable.cs
+++ Surprise18/Assets/Scripts/Interaction/Draggable.cs
@@ -6,194 +6,210 @@ using UnityEngine;
 public class Draggable : MonoBehaviour
 {
 
-    [SerializeField] private Transform originalLocation;
+	public delegate void SnapEvent(Draggable sender, Transform snapLocation);
+	public event SnapEvent OnSnap;
+	public event SnapEvent AbortDragging;
 
-    [SerializeField] private float zDistance = 2f;
+	[SerializeField]
+	private Transform originalLocation;
 
-    [SerializeField] private float lerpFactor = 1f;
+	[SerializeField]
+	private float zDistance = 2f;
 
-
-    private Clickable clickable;
-    private bool dragging = false;
-
-    private Transform snapLocation;
-    private Collider[] activeConstraints;
-    private Rigidbody cursorAnchor;
-    private Vector3 dragPos;
-
-    private const int IDX_CURSOR = 0;
-    private const int IDX_SNAP = 1;
-
-    private Ray currentRay;
+	[SerializeField]
+	private float lerpFactor = 1f;
 
 
-    private void OnEnable()
-    {
-        this.clickable = GetComponent<Clickable>();
-        this.clickable.MouseDown += Clickable_MouseDown;
-        this.activeConstraints = new Collider[2];
+	private Clickable clickable;
+	private bool dragging = false;
 
-        this.snapLocation = this.originalLocation;
-    }
+	private Transform snapLocation;
+	private Collider[] activeConstraints;
+	private Rigidbody cursorAnchor;
+	private Vector3 dragPos;
 
-    private void OnDisable()
-    {
-        if (this.clickable != null)
-        {
-            this.clickable.MouseDown -= Clickable_MouseDown;
-            this.clickable.MouseUp -= Clickable_MouseUp;
-        }
+	private const int IDX_CURSOR = 0;
+	private const int IDX_SNAP = 1;
 
-        this.activeConstraints = null;
-    }
+	private Ray currentRay;
+	private bool eventDirty = false;
 
 
-    private void LateUpdate()
-    {
-        Vector3 targetPos = this.snapLocation.position;
-        Quaternion targetRot = this.snapLocation.rotation;
+	private void OnEnable()
+	{
+		this.clickable = GetComponent<Clickable>();
+		this.clickable.MouseDown += Clickable_MouseDown;
+		this.activeConstraints = new Collider[2];
 
-        if (this.dragging)
-        {
-            // Cancel drag if mouse button not down!
-            if (Input.GetMouseButton(0) == false)
-            {
-                Clickable_MouseUp(null, null);
-                return;
-            }
+		this.snapLocation = this.originalLocation;
+	}
 
+	private void OnDisable()
+	{
+		if (this.clickable != null)
+		{
+			this.clickable.MouseDown -= Clickable_MouseDown;
+			this.clickable.MouseUp -= Clickable_MouseUp;
+		}
 
-            // Update anchor position to match mouse pos
-            Vector3 wsCursorPos = Camera.main.ScreenToWorldPoint(new Vector3(
-                Input.mousePosition.x,
-                Input.mousePosition.y,
-                this.zDistance
-                ));
-            this.dragPos = wsCursorPos;
-            this.cursorAnchor.MovePosition(this.dragPos);
+		this.activeConstraints = null;
+	}
 
 
-            // Do a raycast for any snap targets
-            RaycastHit hit;
-            Vector3 wsMousePos = Input.mousePosition;
-            wsMousePos.z = this.zDistance;
-            this.currentRay = new Ray(Camera.main.transform.position, Camera.main.ScreenToWorldPoint(wsMousePos) - Camera.main.transform.position);
+	private void LateUpdate()
+	{
+		Vector3 targetPos = this.snapLocation.position;
+		Quaternion targetRot = this.snapLocation.rotation;
 
-            if (Physics.Raycast(
-                ray: this.currentRay,
-                hitInfo: out hit,
-                maxDistance: 500f,
-                layerMask: LayerMask.GetMask("SnapCollider")
-                ))
-            {
-                Debug.Log("Hitting collider: " + hit.collider.gameObject.name);
-                if (hit.collider != this.activeConstraints[IDX_SNAP])
-                {
-                    RemoveConstraint(IDX_SNAP);
-                    SetConstraint(IDX_SNAP, hit.collider);
-                }
-            }
-            else
-            {
-                RemoveConstraint(IDX_SNAP);
-            }
+		if (this.dragging)
+		{
+			// Cancel drag if mouse button not down!
+			if (Input.GetMouseButton(0) == false)
+			{
+				Clickable_MouseUp(null, null);
+				return;
+			}
 
 
-            Vector3 blendPos = this.activeConstraints[0].transform.position;
-            Quaternion blendRot = this.activeConstraints[0].transform.rotation;
-
-            // Get inbetween orientation to indicate that there are now two constraints acting upon the draggable
-            if (this.activeConstraints[1] != null)
-            {
-                blendPos = Vector3.Lerp(blendPos, this.activeConstraints[1].transform.position, 0.85f);
-                blendRot = Quaternion.Slerp(blendRot, this.activeConstraints[1].transform.rotation, 0.85f);
-            }
-
-            targetPos = blendPos;
-            targetRot = blendRot;
-        }
-
-        // Lerp the Draggable towards the targetPos/Rot
-        this.transform.position = Vector3.Lerp(this.transform.position, targetPos, this.lerpFactor * Time.deltaTime);
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRot, this.lerpFactor * Time.deltaTime);
-    }
+			// Update anchor position to match mouse pos
+			Vector3 wsCursorPos = Camera.main.ScreenToWorldPoint(new Vector3(
+				Input.mousePosition.x,
+				Input.mousePosition.y,
+				this.zDistance
+				));
+			this.dragPos = wsCursorPos;
+			this.cursorAnchor.MovePosition(this.dragPos);
 
 
-    private void Clickable_MouseDown(Clickable sender, UnityEngine.EventSystems.PointerEventData e)
-    {
-        if (!this.dragging)
-        {
-            this.dragging = true;
+			// Do a raycast for any snap targets
+			RaycastHit hit;
+			Vector3 viewspaceMousePos = Input.mousePosition;
+			viewspaceMousePos.z = this.zDistance;
+			this.currentRay = new Ray(Camera.main.transform.position, Camera.main.ScreenToWorldPoint(viewspaceMousePos) - Camera.main.transform.position);
 
-            GameObject go = new GameObject("_CursorAnchor");
-            this.cursorAnchor = go.AddComponent<Rigidbody>();
-            this.cursorAnchor.isKinematic = true;
-            this.cursorAnchor.useGravity = false;
-            Collider c = go.AddComponent<BoxCollider>();
-
-            SetConstraint(IDX_CURSOR, c);
-        }
-    }
-
-    private void Clickable_MouseUp(Clickable sender, UnityEngine.EventSystems.PointerEventData e)
-    {
-        if (this.dragging)
-        {
-            this.dragging = false;
-            Destroy(this.cursorAnchor.gameObject);
-
-            RemoveConstraint(IDX_CURSOR);
-
-            // Are we currently hovering over a snapCollider? If so, set that as our target position. If not, return to originalLocation
-            if (this.activeConstraints != null && this.activeConstraints[1] != null)
-                this.snapLocation = this.activeConstraints[1].transform;
-            else
-                this.snapLocation = this.originalLocation;
-        }
-    }
+			if (Physics.Raycast(this.currentRay, out hit, 500f, LayerMask.GetMask("SnapCollider")))
+			{
+				if (hit.collider != this.activeConstraints[IDX_SNAP])
+				{
+					RemoveConstraint(IDX_SNAP);
+					SetConstraint(IDX_SNAP, hit.collider);
+				}
+			}
+			else
+			{
+				RemoveConstraint(IDX_SNAP);
+			}
 
 
-    private void SetConstraint(int constraint, Collider other)
-    {
-        if (this.activeConstraints != null)
-        {
-            this.activeConstraints[constraint] = other;
-        }
-    }
+			Vector3 blendPos = this.activeConstraints[0].transform.position;
+			Quaternion blendRot = this.activeConstraints[0].transform.rotation;
 
-    private void RemoveConstraint(int constraint)
-    {
-        if (this.activeConstraints != null)
-        {
-            this.activeConstraints[constraint] = null;
-        }
-    }
+			// Get inbetween orientation to indicate that there are now two constraints acting upon the draggable
+			if (this.activeConstraints[1] != null)
+			{
+				blendPos = Vector3.Lerp(blendPos, this.activeConstraints[1].transform.position, 0.85f);
+				blendRot = Quaternion.Slerp(blendRot, this.activeConstraints[1].transform.rotation, 0.85f);
+			}
+
+			targetPos = blendPos;
+			targetRot = blendRot;
+		}
+
+		// Lerp the Draggable towards the targetPos/Rot
+		this.transform.position = Vector3.Lerp(this.transform.position, targetPos, this.lerpFactor * Time.deltaTime);
+		this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRot, this.lerpFactor * Time.deltaTime);
+
+	}
 
 
-    private void OnGUI()
-    {
-        string s = "Constraints: ";
-        if (this.activeConstraints != null)
-        {
-            if (this.activeConstraints[0] != null)
-                s += "\n[0] " + this.activeConstraints[0].gameObject.name;
+	private void Clickable_MouseDown(Clickable sender, UnityEngine.EventSystems.PointerEventData e)
+	{
+		if (!this.dragging)
+		{
+			this.dragging = true;
 
-            if (this.activeConstraints[1] != null)
-                s += "\n[1] " + this.activeConstraints[1].gameObject.name;
-        }
-        GUI.Label(new Rect(8f, 8f, 128f, 64f), s);
-    }
+			GameObject go = new GameObject("_CursorAnchor");
+			this.cursorAnchor = go.AddComponent<Rigidbody>();
+			this.cursorAnchor.isKinematic = true;
+			this.cursorAnchor.useGravity = false;
+			Collider c = go.AddComponent<BoxCollider>();
 
-    private void OnDrawGizmos()
-    {
-        if (this.activeConstraints != null)
-        {
-            if (this.activeConstraints[0] != null)
-                Gizmos.DrawLine(this.transform.position, this.activeConstraints[0].transform.position);
+			SetConstraint(IDX_CURSOR, c);
+		}
+	}
 
-            if (this.activeConstraints[1] != null)
-                Gizmos.DrawLine(this.transform.position, this.activeConstraints[1].transform.position);
-        }
-    }
+	private void Clickable_MouseUp(Clickable sender, UnityEngine.EventSystems.PointerEventData e)
+	{
+		if (this.dragging)
+		{
+			this.dragging = false;
+			Destroy(this.cursorAnchor.gameObject);
+
+			RemoveConstraint(IDX_CURSOR);
+
+			// Are we currently hovering over a snapCollider? If so, set that as our target position. If not, return to originalLocation
+			if (this.activeConstraints != null && this.activeConstraints[1] != null)
+			{
+				this.snapLocation = this.activeConstraints[1].transform;
+				if (this.OnSnap != null)
+					this.OnSnap.Invoke(this, this.activeConstraints[1].transform);
+			}
+			else
+			{
+				this.snapLocation = this.originalLocation;
+				if (this.AbortDragging != null)
+					this.AbortDragging.Invoke(this, null);
+			}
+		}
+	}
+
+
+	private void SetConstraint(int constraint, Collider other)
+	{
+		if (this.activeConstraints != null)
+		{
+			this.activeConstraints[constraint] = other;
+			this.eventDirty = true;
+		}
+	}
+
+	private void RemoveConstraint(int constraint)
+	{
+		if (this.activeConstraints != null)
+		{
+			this.activeConstraints[constraint] = null;
+		}
+	}
+
+
+	private void OnGUI()
+	{
+		string s = "Constraints: ";
+		if (this.activeConstraints != null)
+		{
+			if (this.activeConstraints[0] != null)
+				s += "\n[0] " + this.activeConstraints[0].gameObject.name;
+
+			if (this.activeConstraints[1] != null)
+				s += "\n[1] " + this.activeConstraints[1].gameObject.name;
+		}
+		GUI.Label(new Rect(8f, 8f, 128f, 64f), s);
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (Application.isPlaying)
+		{
+			if (this.activeConstraints != null)
+			{
+				if (this.activeConstraints[0] != null)
+					Gizmos.DrawLine(this.transform.position, this.activeConstraints[0].transform.position);
+
+				if (this.activeConstraints[1] != null)
+					Gizmos.DrawLine(this.transform.position, this.activeConstraints[1].transform.position);
+			}
+		}
+		
+	}
 
 }
---
+++ Surprise18/Assets/Scripts/Interaction/TestDraggable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Draggable))]
+public class TestDraggable : MonoBehaviour {
+
+
+	void Start()
+	{
+		Draggable d = GetComponent<Draggable>();
+		d.OnSnap += D_OnSnap;
+	}
+
+	private void D_OnSnap(Draggable sender, Transform snapLocation)
+	{
+		Debug.Log("Snapped '" + sender.gameObject.name + "' to '" + snapLocation.gameObject.name + "'");
+	}
+}
